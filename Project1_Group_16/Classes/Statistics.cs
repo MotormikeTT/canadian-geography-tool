9c30e15a1d25d9d49e78a3f01941ef626cf37425
MotormikeTT <motormikett@gmail.com>
2021-02-19 14:33:03 -0400

    Creates Statistics class

---
+++ Project1_Group_16/Classes/Statistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1_Group_16.Classes
+{
+    class Statistics
+    {
+        // Properties
+        private Dictionary<string, CityInfo> CityCatalogue = new Dictionary<string, CityInfo>();
+
+        Statistics(string fileName, string fileType)
+        {
+            // CityCatalogue = DataModeler.ParseFile(fileName, fileType);
+            
+        }
+    }
+}
